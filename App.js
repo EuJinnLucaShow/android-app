8b05792d9040b1963e22fa1f50ce34fcf0753898
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-08-11 17:02:38 +0300

    Form creation

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from 'react-native';
 
-const wallpaper =  require('./assets/images/wallpaper.png');
+const wallpaper = require('./assets/images/wallpaper.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground source={wallpaper} style={styles.image}>       
+      <ImageBackground source={wallpaper} style={styles.image}>
+        <View>
+          <TextInput style={styles.input} placeholder="Логін" />
+          <TextInput
+            style={{ ...styles.input, marginTop: 16, marginBottom: 16 }}
+            placeholder="Адреса електронної пошти"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Пароль"
+            secureTextEntry={true}
+          />
+        </View>
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -15,7 +33,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -26,5 +44,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
     height: '100%',
+  },
+  input: {
+    marginHorizontal: 16,
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+    backgroundColor: '#F6F6F6',
+    borderRadius: 8,
+    paddingHorizontal: 16,
   },
 });
