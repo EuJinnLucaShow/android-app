4f0f168973d4f5b8ef0d73ed3ec472cd6bcdc0af
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-08-14 12:03:06 +0300

    Connecting fonts

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {
   StyleSheet,
@@ -13,6 +13,11 @@ import {
   Platform,
 } from 'react-native';
 
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
+
 const wallpaper = require('./assets/images/wallpaper.png');
 
 const initialState = {
@@ -21,19 +26,59 @@ const initialState = {
   password: '',
 };
 
+const loadFonts = () => {
+  return Font.loadAsync({
+    Roboto: require('./assets/fonts/Roboto-Regular.ttf'),
+  });
+};
+
 export default function App() {
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
   const [state, setState] = useState(initialState);
+  const [appIsReady, setAppIsReady] = useState(false);
 
   const keyboardHide = () => {
     setIsShowKeyboard(false);
     Keyboard.dismiss();
+    console.log(state);
     setState(initialState);
   };
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await loadFonts();
+        // await Font.loadAsync(Entypo.font);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <TouchableWithoutFeedback onPress={keyboardHide}>
-      <View style={styles.container}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
         <ImageBackground source={wallpaper} style={styles.image}>
           <KeyboardAvoidingView
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
