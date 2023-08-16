d3f18e72b1d22d957b49ded0a4c778c718cb0a39
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-08-16 12:36:41 +0300

    Styling createBottomTabNavigator

--- router.js
+++ router.js
@@ -12,6 +12,8 @@ import PostsScreen from './src/screens/mainScreen/PostsScreen';
 import CreatePostsScreen from './src/screens/mainScreen/CreatePostsScreen';
 import ProfileScreen from './src/screens/mainScreen/ProfileScreen';
 
+import { Feather } from '@expo/vector-icons';
+
 export const UseRoute = isAuth => {
   if (!isAuth) {
     return (
@@ -29,11 +31,48 @@ export const UseRoute = isAuth => {
     <MainTab.Navigator
       screenOptions={{
         tabBarShowLabel: false,
+        tabBarStyle: { height: 80 },
       }}
     >
-      <MainTab.Screen name="Posts" component={PostsScreen} />
-      <MainTab.Screen name="Create" component={CreatePostsScreen} />
-      <MainTab.Screen name="Profile" component={ProfileScreen} />
+      <MainTab.Screen
+        name="Posts"
+        component={PostsScreen}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <Feather
+              name="grid"
+              size={focused ? 30 : size}
+              color={focused ? '#FF6C00' : color}
+            />
+          ),
+        }}
+      />
+      <MainTab.Screen
+        name="Create"
+        component={CreatePostsScreen}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <Feather
+              name="plus"
+              size={focused ? 30 : size}
+              color={focused ? '#FF6C00' : color}
+            />
+          ),
+        }}
+      />
+      <MainTab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <Feather
+              name="user"
+              size={focused ? 30 : size}
+              color={focused ? '#FF6C00' : color}
+            />
+          ),
+        }}
+      />
     </MainTab.Navigator>
   );
 };
